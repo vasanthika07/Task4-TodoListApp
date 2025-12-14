cf287c90e28bdf6038451193fb3824f9cf9d7ff6
Vasanthika Jakka <vasanthikajakka@gmail.com>
2025-12-14 23:02:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.onclick = function () {
+    li.remove();
+  };
+
+  li.appendChild(deleteBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  input.value = "";
+}
